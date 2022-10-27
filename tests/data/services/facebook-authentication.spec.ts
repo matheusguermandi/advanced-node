211b2f36c9fcc555b2746283440f2e5f739cd543
matheusguermandi <matheus_guermandi@hotmail.com>
2022-10-27 12:00:47 +0100

    test: ensure Service returns AuthenticationError when LoadFacebookUserApi returns undefined

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,28 +1,40 @@
+import { AuthenticationError } from "@/domain/errors";
 import { FacebookAuthentication } from "@/domain/features";
 
 class FacebookAuthenticationService {
   constructor(private readonly loadFacebookUserApi: LoadFacebookUserApi) {}
 
-  async perform(params: FacebookAuthentication.Params): Promise<void> {
+  async perform(
+    params: FacebookAuthentication.Params
+  ): Promise<AuthenticationError> {
     await this.loadFacebookUserApi.loadUser(params);
+    return new AuthenticationError();
   }
 }
 
 interface LoadFacebookUserApi {
-  loadUser: (params: LoadFacebookUserApi.Params) => Promise<void>;
+  loadUser: (
+    params: LoadFacebookUserApi.Params
+  ) => Promise<LoadFacebookUserApi.Result>;
 }
 
 namespace LoadFacebookUserApi {
   export type Params = {
     token: string;
   };
+
+  export type Result = undefined;
 }
 
 class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
   token?: string;
+  result = undefined;
 
-  async loadUser(params: LoadFacebookUserApi.Params): Promise<void> {
+  async loadUser(
+    params: LoadFacebookUserApi.Params
+  ): Promise<LoadFacebookUserApi.Result> {
     this.token = params.token;
+    return this.result;
   }
 }
 
@@ -34,5 +46,15 @@ describe("FacebookAuthenticationService", () => {
     await sut.perform({ token: "any_token" });
 
     expect(loadFacebookUserApi.token).toBe("any_token");
+  });
+
+  it("should return AuthenticationError when LoadFacebookUserApi returns undefined", async () => {
+    const loadFacebookUserApi = new LoadFacebookUserApiSpy();
+    loadFacebookUserApi.result = undefined;
+    const sut = new FacebookAuthenticationService(loadFacebookUserApi);
+
+    const authResult = await sut.perform({ token: "any_token" });
+
+    expect(authResult).toEqual(new AuthenticationError());
   });
 });
