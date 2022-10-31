27c27575c41a4aa82a25c337006c43ffb8d4a752
matheusguermandi <matheus_guermandi@hotmail.com>
2022-10-31 16:34:06 +0000

    test: update facebook-authentication test with new facebook-authentication service

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,5 +1,8 @@
 import { LoadFacebookUserApi } from "@/data/contracts/apis";
-import { LoadUserAccountRepository } from "@/data/contracts/repos";
+import {
+  LoadUserAccountRepository,
+  CreateFacebookAccountRepository,
+} from "@/data/contracts/repos";
 import { FacebookAuthenticationService } from "@/data/services";
 import { AuthenticationError } from "@/domain/errors";
 
@@ -8,6 +11,7 @@ import { mock, MockProxy } from "jest-mock-extended";
 describe("FacebookAuthenticationService", () => {
   let loadFacebookUserApi: MockProxy<LoadFacebookUserApi>;
   let loadUserAccountRepo: MockProxy<LoadUserAccountRepository>;
+  let createFacebookAccountRepo: MockProxy<CreateFacebookAccountRepository>;
   let sut: FacebookAuthenticationService;
   const token = "any_token";
 
@@ -19,9 +23,11 @@ describe("FacebookAuthenticationService", () => {
       facebookId: "any_fb_id",
     });
     loadUserAccountRepo = mock();
+    createFacebookAccountRepo = mock();
     sut = new FacebookAuthenticationService(
       loadFacebookUserApi,
-      loadUserAccountRepo
+      loadUserAccountRepo,
+      createFacebookAccountRepo
     );
   });
 
