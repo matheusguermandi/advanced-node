76c4e56fc19ecaffa796fb98550dfc87b530124e
matheusguermandi <matheus_guermandi@hotmail.com>
2022-10-31 16:33:11 +0000

    chore: update facebook-authentication service with new user-account repository

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -1,12 +1,16 @@
 import { LoadFacebookUserApi } from "@/data/contracts/apis";
-import { LoadUserAccountRepository } from "@/data/contracts/repos";
+import {
+  LoadUserAccountRepository,
+  CreateFacebookAccountRepository,
+} from "@/data/contracts/repos";
 import { AuthenticationError } from "@/domain/errors";
 import { FacebookAuthentication } from "@/domain/features";
 
 export class FacebookAuthenticationService {
   constructor(
     private readonly loadFacebookUserApi: LoadFacebookUserApi,
-    private readonly loadUserAccountRepo: LoadUserAccountRepository
+    private readonly loadUserAccountRepo: LoadUserAccountRepository,
+    private readonly createFacebookAccountRepo: CreateFacebookAccountRepository
   ) {}
 
   async perform(
@@ -15,6 +19,7 @@ export class FacebookAuthenticationService {
     const fbData = await this.loadFacebookUserApi.loadUser(params);
     if (fbData !== undefined) {
       await this.loadUserAccountRepo.load({ email: fbData.email });
+      await this.createFacebookAccountRepo.createFromFacebook(fbData);
     }
     return new AuthenticationError();
   }
