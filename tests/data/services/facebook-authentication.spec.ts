9c6f1bae39d6fa5a36c4635e4a9243d9d978ed5d
matheusguermandi <matheus_guermandi@hotmail.com>
2022-11-07 17:19:51 +0000

    test:ensure Service update account when LoadUserAccountRepo returns data

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -2,6 +2,7 @@ import { LoadFacebookUserApi } from "@/data/contracts/apis";
 import {
   LoadUserAccountRepository,
   CreateFacebookAccountRepository,
+  UpdateFacebookAccountRepository,
 } from "@/data/contracts/repos";
 import { FacebookAuthenticationService } from "@/data/services";
 import { AuthenticationError } from "@/domain/errors";
@@ -11,7 +12,9 @@ import { mock, MockProxy } from "jest-mock-extended";
 describe("FacebookAuthenticationService", () => {
   let facebookApi: MockProxy<LoadFacebookUserApi>;
   let userAccountRepo: MockProxy<
-    LoadUserAccountRepository & CreateFacebookAccountRepository
+    LoadUserAccountRepository &
+      CreateFacebookAccountRepository &
+      UpdateFacebookAccountRepository
   >;
   let sut: FacebookAuthenticationService;
   const token = "any_token";
@@ -51,7 +54,7 @@ describe("FacebookAuthenticationService", () => {
     expect(userAccountRepo.load).toHaveBeenCalledTimes(1);
   });
 
-  it("should call CreateUserAccountRepo when LoadUserAccountRepo returns undefined", async () => {
+  it("should call CreateFacebookAccountRepo when LoadUserAccountRepo returns undefined", async () => {
     userAccountRepo.load.mockResolvedValueOnce(undefined);
 
     await sut.perform({ token });
@@ -62,5 +65,21 @@ describe("FacebookAuthenticationService", () => {
       facebookId: "any_fb_id",
     });
     expect(userAccountRepo.createFromFacebook).toHaveBeenCalledTimes(1);
+  });
+
+  it("should call UpdateFacebookAccountRepo when LoadUserAccountRepo returns data", async () => {
+    userAccountRepo.load.mockResolvedValueOnce({
+      id: "any_id",
+      name: "any_name",
+    });
+
+    await sut.perform({ token });
+
+    expect(userAccountRepo.updateWithFacebook).toHaveBeenCalledWith({
+      id: "any_id",
+      name: "any_name",
+      facebookId: "any_fb_id",
+    });
+    expect(userAccountRepo.updateWithFacebook).toHaveBeenCalledTimes(1);
   });
 });
