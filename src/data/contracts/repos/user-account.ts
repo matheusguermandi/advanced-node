25e2b4206d559cd1d38090286c153367ce8000b8
matheusguermandi <matheus_guermandi@hotmail.com>
2022-11-08 17:27:33 +0000

    chore: remove implementation details from service

--- src/data/contracts/repos/user-account.ts
+++ src/data/contracts/repos/user-account.ts
@@ -17,29 +17,16 @@ export namespace LoadUserAccountRepository {
       };
 }
 
-export interface CreateFacebookAccountRepository {
-  createFromFacebook: (
-    params: CreateFacebookAccountRepository.Params
+export interface SaveFacebookAccountRepository {
+  saveWithFacebook: (
+    params: SaveFacebookAccountRepository.Params
   ) => Promise<void>;
 }
 
-export namespace CreateFacebookAccountRepository {
+export namespace SaveFacebookAccountRepository {
   export type Params = {
+    id?: string;
     email: string;
-    name: string;
-    facebookId: string;
-  };
-}
-
-export interface UpdateFacebookAccountRepository {
-  updateWithFacebook: (
-    params: UpdateFacebookAccountRepository.Params
-  ) => Promise<void>;
-}
-
-export namespace UpdateFacebookAccountRepository {
-  export type Params = {
-    id: string;
     name: string;
     facebookId: string;
   };
--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -1,8 +1,7 @@
 import { LoadFacebookUserApi } from "@/data/contracts/apis";
 import {
   LoadUserAccountRepository,
-  CreateFacebookAccountRepository,
-  UpdateFacebookAccountRepository,
+  SaveFacebookAccountRepository,
 } from "@/data/contracts/repos";
 import { AuthenticationError } from "@/domain/errors";
 import { FacebookAuthentication } from "@/domain/features";
@@ -11,8 +10,7 @@ export class FacebookAuthenticationService {
   constructor(
     private readonly facebookApi: LoadFacebookUserApi,
     private readonly userAccountRepo: LoadUserAccountRepository &
-      CreateFacebookAccountRepository &
-      UpdateFacebookAccountRepository
+      SaveFacebookAccountRepository
   ) {}
 
   async perform(
@@ -23,15 +21,12 @@ export class FacebookAuthenticationService {
       const accountData = await this.userAccountRepo.load({
         email: fbData.email,
       });
-      if (accountData !== undefined) {
-        await this.userAccountRepo.updateWithFacebook({
-          id: accountData.id,
-          name: accountData.name ?? fbData.name,
-          facebookId: fbData.facebookId,
-        });
-      } else {
-        await this.userAccountRepo.createFromFacebook(fbData);
-      }
+      await this.userAccountRepo.saveWithFacebook({
+        id: accountData?.id,
+        name: accountData?.name ?? fbData.name,
+        email: fbData.email,
+        facebookId: fbData.facebookId,
+      });
     }
     return new AuthenticationError();
   }
