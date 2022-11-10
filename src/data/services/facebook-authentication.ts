1b7ddd735e9e64ee8bcbda78276d1f8486c37757
matheusguermandi <matheus_guermandi@hotmail.com>
2022-11-10 12:07:19 +0000

    chore: add crypto (tokenGenerator) in facebook-authentication service

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -1,4 +1,5 @@
 import { LoadFacebookUserApi } from "@/data/contracts/apis";
+import { TokenGenerator } from "@/data/contracts/crypto";
 import {
   LoadUserAccountRepository,
   SaveFacebookAccountRepository,
@@ -11,7 +12,8 @@ export class FacebookAuthenticationService {
   constructor(
     private readonly facebookApi: LoadFacebookUserApi,
     private readonly userAccountRepo: LoadUserAccountRepository &
-      SaveFacebookAccountRepository
+      SaveFacebookAccountRepository,
+    private readonly crypto: TokenGenerator
   ) {}
 
   async perform(
@@ -23,7 +25,8 @@ export class FacebookAuthenticationService {
         email: fbData.email,
       });
       const fbAccount = new FacebookAccount(fbData, accountData);
-      await this.userAccountRepo.saveWithFacebook(fbAccount);
+      const { id } = await this.userAccountRepo.saveWithFacebook(fbAccount);
+      await this.crypto.generateToken({ key: id });
     }
     return new AuthenticationError();
   }
