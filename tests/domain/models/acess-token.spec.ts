255cc414aeb4d107367b32763e6c111fffe5d994
matheusguermandi <matheus_guermandi@hotmail.com>
2022-11-11 12:14:08 +0000

    test: ensure AccessToken expires in 30 minutes

--- tests/domain/models/acess-token.spec.ts
+++ tests/domain/models/acess-token.spec.ts
@@ -3,7 +3,10 @@ import { AccessToken } from "@/domain/models";
 describe("AccessToken", () => {
   it("should create with a value", () => {
     const sut = new AccessToken("any_value");
-
     expect(sut).toEqual({ value: "any_value" });
+  });
+
+  it("should expire in 1800000 ms", () => {
+    expect(AccessToken.expirationInMs).toBe(1800000);
   });
 });
