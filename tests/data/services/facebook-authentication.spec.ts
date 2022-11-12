5f6c92d23f3179ee6ad8a723c24dcaa9f1d33693
matheusguermandi <matheus_guermandi@hotmail.com>
2022-11-12 16:57:59 +0000

    test: ensure Service returns an AccessToken on success

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -31,10 +31,11 @@ describe("FacebookAuthenticationService", () => {
     });
     userAccountRepo = mock();
     userAccountRepo.load.mockResolvedValue(undefined);
-    userAccountRepo.saveWithFacebook.mockResolvedValueOnce({
+    userAccountRepo.saveWithFacebook.mockResolvedValue({
       id: "any_account_id",
     });
     crypto = mock();
+    crypto.generateToken.mockResolvedValue("any_generated_token");
     sut = new FacebookAuthenticationService(
       facebookApi,
       userAccountRepo,
@@ -88,5 +89,11 @@ describe("FacebookAuthenticationService", () => {
       expirationInMs: AccessToken.expirationInMs,
     });
     expect(crypto.generateToken).toHaveBeenCalledTimes(1);
+  });
+
+  it("should return an AccessToken on success", async () => {
+    const authResult = await sut.perform({ token });
+
+    expect(authResult).toEqual(new AccessToken("any_generated_token"));
   });
 });
