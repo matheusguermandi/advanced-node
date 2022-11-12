c94c34a920fb20d77bf7b3e023c29b96b3e49b8a
matheusguermandi <matheus_guermandi@hotmail.com>
2022-11-12 16:57:35 +0000

    chore: update facebook-authentication service with crypto contract updated

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -8,7 +8,7 @@ import { AuthenticationError } from "@/domain/errors";
 import { FacebookAuthentication } from "@/domain/features";
 import { AccessToken, FacebookAccount } from "@/domain/models";
 
-export class FacebookAuthenticationService {
+export class FacebookAuthenticationService implements FacebookAuthentication {
   constructor(
     private readonly facebookApi: LoadFacebookUserApi,
     private readonly userAccountRepo: LoadUserAccountRepository &
@@ -18,7 +18,7 @@ export class FacebookAuthenticationService {
 
   async perform(
     params: FacebookAuthentication.Params
-  ): Promise<AuthenticationError> {
+  ): Promise<FacebookAuthentication.Result> {
     const fbData = await this.facebookApi.loadUser(params);
     if (fbData !== undefined) {
       const accountData = await this.userAccountRepo.load({
@@ -26,10 +26,11 @@ export class FacebookAuthenticationService {
       });
       const fbAccount = new FacebookAccount(fbData, accountData);
       const { id } = await this.userAccountRepo.saveWithFacebook(fbAccount);
-      await this.crypto.generateToken({
+      const token = await this.crypto.generateToken({
         key: id,
         expirationInMs: AccessToken.expirationInMs,
       });
+      return new AccessToken(token);
     }
     return new AuthenticationError();
   }
