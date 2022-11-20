38efe573b3995fd221bee0817e9fcb1f9cac75d9
matheusguermandi <matheus_guermandi@hotmail.com>
2022-11-20 12:53:30 +0000

    test: ensure PgUserAccountRepository returns undefined if email does not exists

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -50,5 +50,20 @@ describe("PgUserAccountRepository", () => {
 
       expect(account).toEqual({ id: "1" });
     });
+
+    it("should return undefined if email does not exists", async () => {
+      const db = newDb();
+      const connection = await db.adapters.createTypeormConnection({
+        type: "postgres",
+        entities: [PgUser],
+      });
+      await connection.synchronize();
+      const sut = new PgUserAccountRepository();
+
+      const account = await sut.load({ email: "new_email" });
+
+      expect(account).toBeUndefined();
+      await connection.close();
+    });
   });
 });
