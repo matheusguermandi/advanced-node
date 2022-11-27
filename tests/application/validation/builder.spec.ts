ffed29db73c8081cd2cc0f68a25b11a3a3bb206e
matheusguermandi <matheus_guermandi@hotmail.com>
2022-11-27 19:37:40 +0000

    test: add jest config to integration tests

--- tests/application/validation/builder.spec.ts
+++ tests/application/validation/builder.spec.ts
@@ -1,24 +1,19 @@
-import { RequiredStringValidator, Validator } from "@/application/validation";
+import {
+  RequiredStringValidator,
+  ValidationBuilder,
+} from "@/application/validation";
 
-export class ValidationBuilder {
-  private constructor(
-    private readonly value: string,
-    private readonly fieldName: string,
-    private readonly validators: Validator[] = []
-  ) {}
+describe("ValidationBuilder", () => {
+  it("should return a RequiredStringValidator", () => {
+    const validators = ValidationBuilder.of({
+      value: "any_value",
+      fieldName: "any_name",
+    })
+      .required()
+      .build();
 
-  static of(params: { value: string; fieldName: string }): ValidationBuilder {
-    return new ValidationBuilder(params.value, params.fieldName);
-  }
-
-  required(): ValidationBuilder {
-    this.validators.push(
-      new RequiredStringValidator(this.value, this.fieldName)
-    );
-    return this;
-  }
-
-  build(): Validator[] {
-    return this.validators;
-  }
-}
+    expect(validators).toEqual([
+      new RequiredStringValidator("any_value", "any_name"),
+    ]);
+  });
+});
