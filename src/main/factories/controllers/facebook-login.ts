963e6871bdd137189cc3bccea9865a60919cd319
matheusguermandi <matheus_guermandi@hotmail.com>
2022-11-28 21:27:35 +0000

    chore: add controller factory

---
+++ src/main/factories/controllers/facebook-login.ts
@@ -0,0 +1,6 @@
+import { FacebookLoginController } from "@/application/controllers";
+import { makeFacebookAuthenticationService } from "@/main/factories/services";
+
+export const makeFacebookLoginController = (): FacebookLoginController => {
+  return new FacebookLoginController(makeFacebookAuthenticationService());
+};
