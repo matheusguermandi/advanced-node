29ef94af3c26325618315b7cec918c6bdca53838
matheusguermandi <matheus_guermandi@hotmail.com>
2022-12-03 12:01:57 +0000

    chore: merge data layer into domain layer

--- src/domain/services/facebook-authentication.ts
+++ src/domain/services/facebook-authentication.ts
@@ -1,9 +1,9 @@
-import { LoadFacebookUserApi } from "@/data/contracts/apis";
-import { TokenGenerator } from "@/data/contracts/crypto";
+import { LoadFacebookUserApi } from "@/domain/contracts/apis";
+import { TokenGenerator } from "@/domain/contracts/crypto";
 import {
   LoadUserAccountRepository,
   SaveFacebookAccountRepository,
-} from "@/data/contracts/repos";
+} from "@/domain/contracts/repos";
 import { AuthenticationError } from "@/domain/errors";
 import { FacebookAuthentication } from "@/domain/features";
 import { AccessToken, FacebookAccount } from "@/domain/models";
