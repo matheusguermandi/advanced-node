cfa5cc430522de623636b5e1904e75a7ec03cbab
matheusguermandi <matheus_guermandi@hotmail.com>
2022-12-06 15:08:46 +0000

    test: ensure JwtTokenHandler calls verify with correct params

--- tests/infra/crypto/jwt-token-handler.spec.ts
+++ tests/infra/crypto/jwt-token-handler.spec.ts
@@ -36,6 +36,7 @@ describe("JwtTokenHandler", () => {
       expect(fakeJwt.sign).toHaveBeenCalledWith({ key }, secret, {
         expiresIn: 1,
       });
+      expect(fakeJwt.sign).toHaveBeenCalledTimes(1);
     });
 
     it("should return a token", async () => {
@@ -52,6 +53,21 @@ describe("JwtTokenHandler", () => {
       const promise = sut.generateToken({ key, expirationInMs });
 
       await expect(promise).rejects.toThrow(new Error("token_error"));
+    });
+  });
+
+  describe("validateToken", () => {
+    let token: string;
+
+    beforeAll(() => {
+      token = "any_token";
+    });
+
+    it("should call sign with correct params", async () => {
+      await sut.validateToken({ token });
+
+      expect(fakeJwt.verify).toHaveBeenCalledWith(token, secret);
+      expect(fakeJwt.verify).toHaveBeenCalledTimes(1);
     });
   });
 });
