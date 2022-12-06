b76951f1afead7d63a2070815d971dc7232941cf
matheusguermandi <matheus_guermandi@hotmail.com>
2022-12-06 14:58:05 +0000

    test: ensure Authorize calls TokenValidator with correct params

--- src/domain/use-cases/facebook-authentication.ts
+++ src/domain/use-cases/facebook-authentication.ts
@@ -15,14 +15,13 @@ type Setup = (
   userAccountRepo: LoadUserAccountRepository & SaveFacebookAccountRepository,
   crypto: TokenGenerator
 ) => FacebookAuthentication;
-export type FacebookAuthentication = (params: {
-  token: string;
-}) => Promise<{ accessToken: string }>;
+type Input = { token: string };
+type Output = { accessToken: string };
+export type FacebookAuthentication = (params: Input) => Promise<Output>;
 
 export const setupFacebookAuthentication: Setup =
   (facebookApi, userAccountRepo, crypto) => async (params) => {
     const fbData = await facebookApi.loadUser(params);
-
     if (fbData === undefined) throw new AuthenticationError();
 
     const accountData = await userAccountRepo.load({ email: fbData.email });
