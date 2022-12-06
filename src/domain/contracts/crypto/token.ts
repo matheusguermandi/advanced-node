f6e05c4423037c45bbf00aa42b47e504514c805f
matheusguermandi <matheus_guermandi@hotmail.com>
2022-12-06 15:00:33 +0000

    chore: move Authorize to its own file

--- src/domain/contracts/crypto/token.ts
+++ src/domain/contracts/crypto/token.ts
@@ -12,3 +12,14 @@ export namespace TokenGenerator {
 
   export type Result = string;
 }
+
+export interface TokenValidator {
+  validateToken: (
+    params: TokenValidator.Params
+  ) => Promise<TokenValidator.Result>;
+}
+
+export namespace TokenValidator {
+  export type Params = { token: string };
+  export type Result = string;
+}
