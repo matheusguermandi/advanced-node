cc7f257b9b04ba7c6a6e38942fb71f3203da9b32
matheusguermandi <matheus_guermandi@hotmail.com>
2022-12-07 16:42:41 +0000

    test: ensure AuthenticationMiddleware returns 403 if authorize throws

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -16,7 +16,11 @@ class AuthenticationMiddleware {
       "authorization"
     ).validate();
     if (error !== undefined) return forbidden();
-    await this.authorize({ token: authorization });
+    try {
+      await this.authorize({ token: authorization });
+    } catch {
+      return forbidden();
+    }
   }
 }
 
@@ -66,5 +70,16 @@ describe("AuthenticationMiddleware", () => {
 
     expect(authorize).toHaveBeenCalledWith({ token: authorization });
     expect(authorize).toHaveBeenCalledTimes(1);
+  });
+
+  it("should return 403 if authorize throws", async () => {
+    authorize.mockRejectedValueOnce(new Error("any_error"));
+
+    const httpResponse = await sut.handle({ authorization });
+
+    expect(httpResponse).toEqual({
+      statusCode: 403,
+      data: new ForbiddenError(),
+    });
   });
 });
