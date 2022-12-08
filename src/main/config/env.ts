1bab4567f10968694e07c0152d7e2d909aad1af2
matheusguermandi <matheus_guermandi@hotmail.com>
2022-12-08 19:48:14 +0000

    chore: move sensitive data to dotenv

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,6 +3,9 @@ export const env = {
     clientId: process.env.FB_CLIENT_ID ?? "255017709763229",
     clientSecret:
       process.env.FB_CLIENT_SECRET ?? "c02b1e197ee6aec2947b193148cdaef7",
+    accessToken:
+      process.env.FB_ACCESS_TOKEN ??
+      "EAADn7ZCAAep0BAC1nu5NtvNtzEHt5lUbZC8RSpOjp1dmZArJdHWStRfIhGG5OTPXLz1dZCPs3lADAghqhrruZB2NVX2cXm1ZCNMDITH75F6HYdcmuzp84oWwo1O7ZC84AvBuHzEDxPivsc0fYWupGoyDwfCZAWPUd3MedFp4AADpQUUHQq4TTXVeK6pktPicTxhdi114NScAzwRN5vnrGqmK",
   },
   port: process.env.PORT ?? 8080,
   jwtSecret: process.env.JWT_SECRET ?? "3jk24h32jk4h",
