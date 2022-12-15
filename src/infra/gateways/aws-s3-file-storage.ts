629484f3592983c6b29fbc283559464c6bf55a6e
matheusguermandi <matheus_guermandi@hotmail.com>
2022-12-15 17:48:28 +0000

    test: ensure AwsS3FileStorage calls deleteObject with correct input

--- src/infra/gateways/aws-s3-file-storage.ts
+++ src/infra/gateways/aws-s3-file-storage.ts
@@ -1,8 +1,8 @@
-import { UploadFile } from "@/domain/contracts/gateways";
+import { UploadFile, DeleteFile } from "@/domain/contracts/gateways";
 
 import { config, S3 } from "aws-sdk";
 
-export class AwsS3FileStorage implements UploadFile {
+export class AwsS3FileStorage implements UploadFile, DeleteFile {
   constructor(
     accessKey: string,
     secret: string,
@@ -27,5 +27,15 @@ export class AwsS3FileStorage implements UploadFile {
       })
       .promise();
     return `https://${this.bucket}.s3.amazonaws.com/${encodeURIComponent(key)}`;
+  }
+
+  async delete({ key }: DeleteFile.Input): Promise<void> {
+    const s3 = new S3();
+    await s3
+      .deleteObject({
+        Bucket: this.bucket,
+        Key: key,
+      })
+      .promise();
   }
 }
