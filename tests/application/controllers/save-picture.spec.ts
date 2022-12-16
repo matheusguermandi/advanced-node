e5eb17ba57739b427f5c497aa73d48224f1069f0
matheusguermandi <matheus_guermandi@hotmail.com>
2022-12-16 15:44:39 +0000

    test: ensure SavePictureController accepts valid mime types

--- tests/application/controllers/save-picture.spec.ts
+++ tests/application/controllers/save-picture.spec.ts
@@ -1,17 +1,41 @@
 import { RequiredFieldError } from "@/application/errors";
 import { badRequest, HttpResponse } from "@/application/helpers";
 
-type HttpRequest = { file: { buffer: Buffer } };
+type HttpRequest = { file: { buffer: Buffer; mimeType: string } };
 type Model = Error;
 
 class SavePictureController {
-  async handle({ file }: HttpRequest): Promise<HttpResponse<Model>> {
-    return badRequest(new RequiredFieldError("file"));
+  async handle({
+    file,
+  }: HttpRequest): Promise<HttpResponse<Model> | undefined> {
+    if (file === undefined || file === null) {
+      return badRequest(new RequiredFieldError("file"));
+    }
+    if (file.buffer.length === 0) {
+      return badRequest(new RequiredFieldError("file"));
+    }
+    if (!["image/png", "image/jpg", "image/jpeg"].includes(file.mimeType)) {
+      return badRequest(new InvalidMymeTypeError(["png", "jpeg"]));
+    }
+  }
+}
+
+class InvalidMymeTypeError extends Error {
+  constructor(allowed: string[]) {
+    super(`Unsupported type. Allowed types: ${allowed.join(", ")}`);
+    this.name = "InvalidMymeTypeError";
   }
 }
 
 describe("SavePictureController", () => {
+  let buffer: Buffer;
+  let mimeType: string;
   let sut: SavePictureController;
+
+  beforeAll(() => {
+    buffer = Buffer.from("any_buffer");
+    mimeType = "image/png";
+  });
 
   beforeEach(() => {
     sut = new SavePictureController();
@@ -37,12 +61,56 @@ describe("SavePictureController", () => {
 
   it("should return 400 if file is empty", async () => {
     const httpResponse = await sut.handle({
-      file: { buffer: Buffer.from("") },
+      file: { buffer: Buffer.from(""), mimeType },
     });
 
     expect(httpResponse).toEqual({
       statusCode: 400,
       data: new RequiredFieldError("file"),
+    });
+  });
+
+  it("should return 400 if file type is invalid", async () => {
+    const httpResponse = await sut.handle({
+      file: { buffer, mimeType: "invalid_type" },
+    });
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new InvalidMymeTypeError(["png", "jpeg"]),
+    });
+  });
+
+  it("should not return 400 if file type is valid", async () => {
+    const httpResponse = await sut.handle({
+      file: { buffer, mimeType: "image/png" },
+    });
+
+    expect(httpResponse).not.toEqual({
+      statusCode: 400,
+      data: new InvalidMymeTypeError(["png", "jpeg"]),
+    });
+  });
+
+  it("should not return 400 if file type is valid", async () => {
+    const httpResponse = await sut.handle({
+      file: { buffer, mimeType: "image/jpg" },
+    });
+
+    expect(httpResponse).not.toEqual({
+      statusCode: 400,
+      data: new InvalidMymeTypeError(["png", "jpeg"]),
+    });
+  });
+
+  it("should not return 400 if file type is valid", async () => {
+    const httpResponse = await sut.handle({
+      file: { buffer, mimeType: "image/jpeg" },
+    });
+
+    expect(httpResponse).not.toEqual({
+      statusCode: 400,
+      data: new InvalidMymeTypeError(["png", "jpeg"]),
     });
   });
 });
