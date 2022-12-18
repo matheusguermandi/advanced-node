288605082280c2aec74fbc01e5a17dceab853cc1
matheusguermandi <matheus_guermandi@hotmail.com>
2022-12-18 21:04:10 +0000

    chore: add Validators to builder

--- src/application/controllers/save-picture.ts
+++ src/application/controllers/save-picture.ts
@@ -1,10 +1,7 @@
 import { Controller } from "@/application/controllers";
 import { HttpResponse, ok } from "@/application/helpers";
 import {
-  AllowedMimeTypes,
-  MaxFileSize,
-  Required,
-  RequiredBuffer,
+  ValidationBuilder as Builder,
   Validator,
 } from "@/application/validation";
 import { ChangeProfilePicture } from "@/domain/use-cases";
@@ -33,10 +30,10 @@ export class SavePictureController extends Controller {
 
   override buildValidators({ file }: HttpRequest): Validator[] {
     return [
-      new Required(file, "file"),
-      new RequiredBuffer(file.buffer, "file"),
-      new AllowedMimeTypes(["png", "jpg"], file.mimeType),
-      new MaxFileSize(5, file.buffer),
+      ...Builder.of({ value: file, fieldName: "file" })
+        .required()
+        .image({ allowed: ["png", "jpg"], maxSizeInMb: 5 })
+        .build(),
     ];
   }
 }
--- src/application/validation/builder.ts
+++ src/application/validation/builder.ts
@@ -1,9 +1,17 @@
-import { RequiredString, Validator } from "@/application/validation";
+import {
+  Validator,
+  AllowedMimeTypes,
+  Extension,
+  MaxFileSize,
+  Required,
+  RequiredBuffer,
+  RequiredString,
+} from "@/application/validation";
 
 export class ValidationBuilder {
   private constructor(
-    private readonly value: string,
-    private readonly fieldName: string,
+    private readonly value: any,
+    private readonly fieldName?: string,
     private readonly validators: Validator[] = []
   ) {}
 
@@ -11,14 +19,41 @@ export class ValidationBuilder {
     value,
     fieldName,
   }: {
-    value: string;
-    fieldName: string;
+    value: any;
+    fieldName?: string;
   }): ValidationBuilder {
     return new ValidationBuilder(value, fieldName);
   }
 
   required(): ValidationBuilder {
-    this.validators.push(new RequiredString(this.value, this.fieldName));
+    if (this.value instanceof Buffer) {
+      this.validators.push(new RequiredBuffer(this.value, this.fieldName));
+    } else if (typeof this.value === "string") {
+      this.validators.push(new RequiredString(this.value, this.fieldName));
+    } else if (typeof this.value === "object") {
+      this.validators.push(new Required(this.value, this.fieldName));
+      if (this.value.buffer !== undefined) {
+        this.validators.push(
+          new RequiredBuffer(this.value.buffer, this.fieldName)
+        );
+      }
+    }
+    return this;
+  }
+
+  image({
+    allowed,
+    maxSizeInMb,
+  }: {
+    allowed: Extension[];
+    maxSizeInMb: number;
+  }): ValidationBuilder {
+    if (this.value.mimeType !== undefined) {
+      this.validators.push(new AllowedMimeTypes(allowed, this.value.mimeType));
+    }
+    if (this.value.buffer !== undefined) {
+      this.validators.push(new MaxFileSize(maxSizeInMb, this.value.buffer));
+    }
     return this;
   }
 
