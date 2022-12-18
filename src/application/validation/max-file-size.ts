090f702083ab0f833a473d7f415ec3440253fb6e
matheusguermandi <matheus_guermandi@hotmail.com>
2022-12-18 21:01:46 +0000

    chore: add MaxFileSize Validator

---
+++ src/application/validation/max-file-size.ts
@@ -0,0 +1,15 @@
+/* eslint-disable curly */
+import { MaxFileSizeError } from "../errors";
+
+export class MaxFileSize {
+  constructor(
+    private readonly maxSizeInMb: number,
+    private readonly value: Buffer
+  ) {}
+
+  validate(): Error | undefined {
+    const maxFileSizeInBytes = 5 * 1024 * 1024;
+    if (this.value.length > maxFileSizeInBytes)
+      return new MaxFileSizeError(this.maxSizeInMb);
+  }
+}
