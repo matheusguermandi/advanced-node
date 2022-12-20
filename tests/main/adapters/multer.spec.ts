c769ce7f6271a938aceebe5238d7bdf2f9bab4aa
matheusguermandi <matheus_guermandi@hotmail.com>
2022-12-20 21:01:52 +0000

    test: ensure MulterAdapter calls next on success

--- tests/main/adapters/multer.spec.ts
+++ tests/main/adapters/multer.spec.ts
@@ -18,6 +18,8 @@ const adaptMulter: RequestHandler = (req, res, next) => {
         file: { buffer: req.file.buffer, mimeType: req.file.mimetype },
       };
     }
+
+    next();
   });
 };
 
@@ -96,5 +98,12 @@ describe("MulterAdapter", () => {
         mimeType: req.file?.mimetype,
       },
     });
+  });
+
+  it("should call next on success", () => {
+    sut(req, res, next);
+
+    expect(next).toHaveBeenCalledWith();
+    expect(next).toHaveBeenCalledTimes(1);
   });
 });
