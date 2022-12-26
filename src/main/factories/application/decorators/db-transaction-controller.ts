9463047beddfe172cdc0a76b2168e20e81ed1130
matheusguermandi <matheus_guermandi@hotmail.com>
2022-12-26 12:17:24 +0000

    chore: add PgTransactionController factory

---
+++ src/main/factories/application/decorators/db-transaction-controller.ts
@@ -0,0 +1,9 @@
+import { Controller } from "@/application/controllers";
+import { DbTransactionController } from "@/application/decorators";
+import { makePgConnection } from "@/main/factories/infra/repos/postgres/helpers";
+
+export const makePgTransactionController = (
+  controller: Controller
+): DbTransactionController => {
+  return new DbTransactionController(controller, makePgConnection());
+};
